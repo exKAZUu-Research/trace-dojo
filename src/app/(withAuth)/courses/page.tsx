9809d1ade33e608c5242c2e5ccf56633ddd9ff7a
Sakamoto, Kazunori <exkazuu@gmail.com>
2024-01-12 19:33:44 +0900

    .

--- src/app/(withAuth)/courses/page.tsx
+++ src/app/(withAuth)/courses/page.tsx
@@ -1,16 +1,28 @@
+import { Link } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import NextLink from 'next/link';
 
+import { courseIds, courseIdToName } from '../../../problems/problemData';
 import { getNonNullableSessionOnServer } from '../../../utils/session';
 
-const CoursePage: NextPage = async () => {
+const CoursesPage: NextPage = async () => {
   const session = await getNonNullableSessionOnServer();
 
   return (
     <main>
       <div>UserID: {session.getUserId()}</div>
       <h1>Courses</h1>
+      <ul>
+        {courseIds.map((id) => (
+          <li key={id}>
+            <Link as={NextLink} href={`/courses/${id}`}>
+              {courseIdToName[id]}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
 
-export default CoursePage;
+export default CoursesPage;
