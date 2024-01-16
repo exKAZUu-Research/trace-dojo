a06167b52d8b72cb6009e00f02fbc32388c938c9
Kento Sato <tatehito.st@gmail.com>
2024-01-16 11:30:51 +0900

    Add generateProgram function and display problem statement

--- src/app/(withAuth)/problems/[problemId]/page.tsx
+++ src/app/(withAuth)/problems/[problemId]/page.tsx
@@ -2,7 +2,7 @@ import { Box, Button, Flex, HStack, Heading, VStack } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 import { TurtleGraphics } from '../../../../components/organisms/TurtleGraphics';
-import { programIdToName } from '../../../../problems/problemData';
+import { programIdToName, generateProgram } from '../../../../problems/problemData';
 
 const GRID_COLUMNS = 12;
 const GRID_ROWS = 8;
@@ -15,7 +15,7 @@ const ProblemPage: NextPage<{ params: { problemId: string } }> = async ({ params
         <Heading as="h1">{programIdToName[params.problemId]}</Heading>
         <Flex gap="10" w="100%">
           <VStack spacing="10" w={GRID_COLUMNS * GRID_SIZE}>
-            <Box>下の図のように1辺の長さが3のを描くために穴に入れるべきプログラムを選択してください</Box>
+            <Box>ここに問題文を表示する</Box>
             <Box>
               <TurtleGraphics characters={[]} gridColumns={GRID_COLUMNS} gridRows={GRID_ROWS} gridSize={GRID_SIZE} />
             </Box>
@@ -24,6 +24,7 @@ const ProblemPage: NextPage<{ params: { problemId: string } }> = async ({ params
             <Button colorScheme="gray">解説</Button>
             <Box border="1px" h="500" p="4" w="100%">
               ここにエディタを表示する
+              <pre>{generateProgram(params.problemId, 'java')}</pre>
             </Box>
             <HStack>
               <Button colorScheme="gray">リセット</Button>
