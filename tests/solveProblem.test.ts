8936bb8e2de7284a4e7b2b3267daf39163766c7b
Yuki Ito <37338201+ykit00@users.noreply.github.com>
2024-01-24 16:03:21 +0900

    feat: implement solveProblem test

--- tests/solveProblem.test.ts
+++ tests/solveProblem.test.ts
@@ -1,37 +1,40 @@
 import { expect, test } from 'vitest';
 
-import { Character } from '../src/app/lib/Character';
-import { solveProblem, parseProgram, createBoard } from '../src/app/lib/solveProblem';
+import { solveProblem, parseProgram } from '../src/app/lib/solveProblem';
 
 test('Parse a program', () => {
   const program = `
-    const turtle = new Turtle();
-    turtle.moveForward(5);
+    const character = new Character();
+    character.moveForward();
+    character.moveForward();
+    character.moveForward();
+    character.moveForward();
+    character.moveForward();
   `;
   const parsedProgram = parseProgram(program);
 
   expect(parsedProgram).not.toBeFalsy();
-  expect(parsedProgram).toEqual(['const turtle = new Turtle();', 'turtle.moveForward(5);']);
+  expect(parsedProgram).toEqual([
+    'const character = new Character();',
+    'character.moveForward();',
+    'character.moveForward();',
+    'character.moveForward();',
+    'character.moveForward();',
+    'character.moveForward();',
+  ]);
 });
 
 test('Solve a problem', () => {
   const problemProgram = `
-    const turtle = new Turtle();
-    turtle.moveForward(5);
+    const character = new Character();
+    character.moveForward();
+    character.moveForward();
+    character.moveForward();
+    character.moveForward();
+    character.moveForward();
   `;
-  const character = new Character({
-    name: 'Turtle',
-    id: 1,
-    x: 1,
-    y: 1,
-    path: ['1,1'],
-    direction: 'down',
-    color: 'red',
-    penDown: true,
-  });
-  const board = createBoard(8, 12);
 
-  const answer = solveProblem(problemProgram, character, board);
+  const answer = solveProblem(problemProgram);
 
   expect(answer).not.toBeFalsy();
   expect(answer.character).not.toBeFalsy();
@@ -39,15 +42,22 @@ test('Solve a problem', () => {
   expect(answer.character.path).toEqual([1, 6]);
   expect(answer.character.direction).toEqual('down');
   expect(answer.character.penDown).toEqual(true);
+
   // prettier-ignore
   expect(answer.board.grid).toEqual([
-    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
-    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
-    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
-    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
-    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
-    [{ color: 'red' }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
-    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
-    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: 'red' }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: 'red' }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: 'red' }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: 'red' }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: 'red' }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: 'red' }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
+    [{ color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }, { color: undefined }],
   ]);
+  expect(answer.histories).not.toBeFalsy();
+  expect(answer.histories?.at(-1)?.character.path).toEqual([1, 6]);
 });
