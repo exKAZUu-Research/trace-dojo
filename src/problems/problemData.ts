c5c07d7734ba0c8f1cb2e008de6266f344cf7bcd
taisii <taisii2252@gmail.com>
2024-02-15 16:33:19 +0900

    .

--- src/problems/problemData.ts
+++ src/problems/problemData.ts
@@ -36,14 +36,32 @@ export const courseIdToProgramIdLists: Record<CourseId, ProgramId[][]> = {
 
 export function generateProgram(programId: ProgramId, languageId: LanguageId): string {
   // TODO(exKAZUu): 問題IDに紐づくプログラム（テンプレート）を取得して、乱数を使って具体的なプログラムを生成する。
-  return (
-    `const character1 = new Character();
-character1.moveForward();
-character1.moveForward();
-character1.moveForward();
-character1.moveForward();
-character1.moveForward();` || programIdToLanguageIdToProgram[programId][languageId]
+  console.log('languageId', languageId);
+  const randomNumberRegex = /<(\d+)-(\d+)>/g;
+  const programTemplete = programIdToLanguageIdToProgram[programId];
+  const jsTemplete = programTemplete['js'];
+  const randomNumberArray: number[] = [];
+  const jsProgram = jsTemplete.replaceAll(randomNumberRegex, (match, min, max) => {
+    const randomNumber = getRandomInt(Number(min), Number(max));
+    randomNumberArray.push(randomNumber);
+    return randomNumber.toString();
+  });
+
+  let index = 0;
+  const displayProgram = programTemplete['java'].replaceAll(randomNumberRegex, () =>
+    randomNumberArray[index++].toString()
   );
+  const generateProgram = {
+    displayProgram,
+    excuteProgram: jsProgram,
+  };
+  return generateProgram.excuteProgram;
+}
+
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 export function getExplanation(programId: ProgramId, languageId: LanguageId): string {
@@ -53,32 +71,46 @@ export function getExplanation(programId: ProgramId, languageId: LanguageId): st
 export const programIdToLanguageIdToProgram: Record<ProgramId, Record<LanguageId, string>> = {
   straight: {
     js: `
-const turtle = new Turtle();
-turtle.moveForward(<3-5>);
+const turtle1 = new Character();
+turtle1.moveForward();
+turtle1.moveForward();
+turtle1.moveForward();
+turtle1.moveForward();
 `.trim(),
     java: `
 public class Straight {
   public static void main(String[] args) {
-    var turtle = new Turtle();
-    turtle.moveForward(<3-5>);
+    var turtle1 = new Character();
+    turtle1.moveForward();
+    turtle1.moveForward();
+    turtle1.moveForward();
+    turtle1.moveForward();
   }
 }
 `.trim(),
   },
   curve: {
     js: `
-const turtle = new Turtle();
-turtle.moveForward(<3-5>);
+const turtle = new Character();
+for (let i = 0; i < <3-10>; i++) {
+  turtle.moveForward();
+}
 turtle.turnLeft();
-turtle.moveForward(<3-5>);
+for (let i = 0; i < <3-5>; i++) {
+  turtle.moveForward();
+}
 `.trim(),
     java: `
 public class Curve {
   public static void main(String[] args) {
-    var turtle = new Turtle();
-    turtle.moveForward(<3-5>);
+    var turtle = new Character();
+    for (int i = 0; i < <3-10>; i++) {
+      turtle.moveForward();
+    }
     turtle.turnLeft();
-    turtle.moveForward(<3-5>);
+    for (int i = 0; i < <3-5>; i++) {
+      turtle.moveForward();
+    }
   }
 }
 `.trim(),
