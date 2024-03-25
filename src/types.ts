d0e920d5f4c755d582151cafbe2baf8e92915536
Kento Sato <tatehito.st@gmail.com>
2024-03-25 16:42:52 +0900

    Remove unnecessary code

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,5 @@
 import type React from 'react';
 
-import type { Board } from './app/lib/board';
-import type { Character } from './app/lib/character';
-
 export interface LayoutProps {
   children: React.ReactNode;
 }
@@ -11,32 +8,10 @@ export type LayoutComponent = React.FC<LayoutProps>;
 
 export type ColorChar = '#' | '.' | 'R' | 'B' | 'G' | 'Y' | 'P';
 export type CellColor = 'black' | 'white' | 'red' | 'blue' | 'green' | 'yellow' | 'purple';
-export type Cell = {
-  color: CellColor;
-};
+
 export type SelectedCell = {
   x: number;
   y: number;
 };
-
-export type CharacterVariable = { name: string; value: Character };
-
-export type Variable = { name: string; value: string };
-
-export type History = {
-  step: number;
-  characterVariables: CharacterVariable[] | undefined;
-  board: Board;
-  otherVariables: Variable[];
-};
-
-export type SolveProblemResult = {
-  characterVariables: CharacterVariable[] | undefined;
-  otherVariables: Variable[] | undefined;
-  board: Board;
-  histories: History[] | undefined;
-};
-
-export type CharacterDirection = 'up' | 'down' | 'left' | 'right';
 
 export type ProblemType = 'executionResult' | 'checkpoint' | 'step';
