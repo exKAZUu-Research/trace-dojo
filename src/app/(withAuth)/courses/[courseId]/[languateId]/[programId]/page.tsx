dc9a4b414e093aac03800d7ae79480810b9c0181
Yuki Ito <37338201+ykit00@users.noreply.github.com>
2024-03-28 15:51:01 +0900

    refactor: fetch problem session in server component

--- src/app/(withAuth)/courses/[courseId]/[languateId]/[programId]/page.tsx
+++ src/app/(withAuth)/courses/[courseId]/[languateId]/[programId]/page.tsx
@@ -1,14 +1,68 @@
+import type { UserProblemSession } from '@prisma/client';
 import type { NextPage } from 'next';
 
-import type { CourseId, ProgramId } from '../../../../../../problems/problemData';
+import type { CourseId, ProgramId, VisibleLanguageId } from '../../../../../../problems/problemData';
 import { getNonNullableSessionOnServer } from '../../../../../../utils/session';
+import { getSuspendedUserProblemSession, upsertUserProblemSession } from '../../../../../lib/actions';
 
 import { BaseProblem } from './BaseProblem';
 
-const ProblemPage: NextPage<{ params: { courseId: CourseId; programId: ProgramId } }> = async ({ params }) => {
+const ProblemPage: NextPage<{
+  params: { courseId: CourseId; languateId: VisibleLanguageId; programId: ProgramId };
+}> = async ({ params }) => {
   const session = await getNonNullableSessionOnServer();
+  const userId = session.getUserId();
+  const courseId = params.courseId;
+  // TODO: fix typo
+  const languageId = params.languateId;
+  const programId = params.programId;
 
-  return <BaseProblem courseId={params.courseId} programId={params.programId} userId={session.getUserId()} />;
+  const suspendedSession: UserProblemSession | undefined = await getSuspendedUserProblemSession(
+    userId,
+    courseId,
+    programId,
+    languageId
+  );
+
+  let userProblemSession = suspendedSession;
+
+  if (!userProblemSession) {
+    const problemVariableSeed = Date.now().toString();
+    const problemType = 'executionResult';
+    const startedAt = new Date();
+
+    userProblemSession = await upsertUserProblemSession(
+      // createするためにidに0を指定
+      0,
+      userId,
+      courseId,
+      programId,
+      languageId,
+      problemVariableSeed,
+      problemType,
+      0,
+      0,
+      undefined,
+      startedAt,
+      undefined,
+      false
+    );
+  }
+
+  console.log('languageId:', languageId);
+  console.log('userProblemSession:', userProblemSession);
+
+  if (!userProblemSession) return <>undefined</>;
+
+  return (
+    <BaseProblem
+      courseId={params.courseId}
+      languageId={languageId}
+      programId={params.programId}
+      userId={session.getUserId()}
+      userProblemSession={userProblemSession}
+    />
+  );
 };
 
 export default ProblemPage;
