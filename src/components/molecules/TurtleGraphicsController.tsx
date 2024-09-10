99ffe23ac0d89d00f24cd812dcd163e17ac3d2a7
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-09-10 10:20:09 +0900

    remove color select button

--- src/components/molecules/TurtleGraphicsController.tsx
+++ src/components/molecules/TurtleGraphicsController.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 
-import { TURTLE_GRAPHICS_DEFAULT_COLOR as DEFAULT_COLOR } from '../../constants';
-import { Box, Button, HStack, Select, VStack } from '../../infrastructures/useClient/chakra';
+import { Box, Button, HStack, VStack } from '../../infrastructures/useClient/chakra';
 import type { CharacterTrace } from '../../problems/traceProgram';
-import type { ColorChar, SelectedCell } from '../../types';
+import type { SelectedCell } from '../../types';
 
 interface TurtleGraphicsControllerProps {
-  board: ColorChar[][];
   selectedCharacter?: CharacterTrace;
   selectedCell?: SelectedCell;
-  handleChangeCellColorButton: (color: ColorChar) => void;
-  handleAddCharacterButton: (color: ColorChar) => void;
+  handleAddCharacterButton: () => void;
   handleRemoveCharacterButton: (character: CharacterTrace) => void;
   handleClickCharacterTurnLeftButton: () => void;
   handleClickCharacterTurnRightButton: () => void;
@@ -22,9 +19,7 @@ interface TurtleGraphicsControllerProps {
 }
 
 export const TurtleGraphicsController: React.FC<TurtleGraphicsControllerProps> = ({
-  board,
   handleAddCharacterButton,
-  handleChangeCellColorButton,
   handleClickCharacterMoveForwardButton,
   handleClickCharacterPenDownButton,
   handleClickCharacterPenUpButton,
@@ -34,13 +29,6 @@ export const TurtleGraphicsController: React.FC<TurtleGraphicsControllerProps> =
   selectedCell,
   selectedCharacter,
 }) => {
-  const [selectedCharacterColor, setSelectedCharacterColor] = useState<ColorChar>(DEFAULT_COLOR);
-
-  const handleSelectCharacterColor = (event: React.ChangeEvent<HTMLSelectElement>): void => {
-    const inputValue = event.target.value;
-    setSelectedCharacterColor(inputValue as ColorChar);
-  };
-
   return (
     <VStack justifyContent="center" marginTop="4" spacing="4">
       {selectedCharacter && (
@@ -67,77 +55,11 @@ export const TurtleGraphicsController: React.FC<TurtleGraphicsControllerProps> =
       )}
 
       {selectedCell && (
-        <>
-          <HStack>
-            <Box>
-              <Button onClick={() => handleAddCharacterButton(selectedCharacterColor)}>キャラクターを追加する</Button>
-            </Box>
-            <Select maxW="300" value={selectedCharacterColor} onChange={(e) => handleSelectCharacterColor(e)}>
-              <option key={'#'} value={'#'}>
-                黒
-              </option>
-              <option key={'R'} value={'R'}>
-                赤
-              </option>
-              <option key={'B'} value={'B'}>
-                青
-              </option>
-              <option key={'G'} value={'G'}>
-                緑
-              </option>
-              <option key={'Y'} value={'Y'}>
-                黄
-              </option>
-              <option key={'P'} value={'P'}>
-                紫
-              </option>
-            </Select>
-          </HStack>
-          <HStack>
-            <Button
-              backgroundColor={'black'}
-              border={'1px'}
-              borderColor={'gray.400'}
-              opacity={'#' === (board[selectedCell.y][selectedCell.x] as ColorChar) ? '1' : '0.3'}
-              onClick={() => handleChangeCellColorButton('#')}
-            ></Button>
-            <Button
-              backgroundColor={'red'}
-              border={'1px'}
-              borderColor={'gray.400'}
-              opacity={'R' === (board[selectedCell.y][selectedCell.x] as ColorChar) ? '1' : '0.3'}
-              onClick={() => handleChangeCellColorButton('R')}
-            ></Button>
-            <Button
-              backgroundColor={'blue'}
-              border={'1px'}
-              borderColor={'gray.400'}
-              opacity={'B' === (board[selectedCell.y][selectedCell.x] as ColorChar) ? '1' : '0.3'}
-              onClick={() => handleChangeCellColorButton('B')}
-            ></Button>
-            <Button
-              backgroundColor={'green'}
-              border={'1px'}
-              borderColor={'gray.400'}
-              opacity={'G' === (board[selectedCell.y][selectedCell.x] as ColorChar) ? '1' : '0.3'}
-              onClick={() => handleChangeCellColorButton('G')}
-            ></Button>
-            <Button
-              backgroundColor={'yellow'}
-              border={'1px'}
-              borderColor={'gray.400'}
-              opacity={'Y' === (board[selectedCell.y][selectedCell.x] as ColorChar) ? '1' : '0.3'}
-              onClick={() => handleChangeCellColorButton('Y')}
-            ></Button>
-            <Button
-              backgroundColor={'purple'}
-              border={'1px'}
-              borderColor={'gray.400'}
-              opacity={'P' === (board[selectedCell.y][selectedCell.x] as ColorChar) ? '1' : '0.3'}
-              onClick={() => handleChangeCellColorButton('P')}
-            ></Button>
-          </HStack>
-        </>
+        <HStack>
+          <Box>
+            <Button onClick={() => handleAddCharacterButton()}>キャラクターを追加する</Button>
+          </Box>
+        </HStack>
       )}
     </VStack>
   );
--- src/components/organisms/TurtleGraphics.tsx
+++ src/components/organisms/TurtleGraphics.tsx
@@ -6,6 +6,7 @@ import {
   TURTLE_GRAPHICS_GRID_COLUMNS as GRID_COLUMNS,
   TURTLE_GRAPHICS_GRID_ROWS as GRID_ROWS,
   TURTLE_GRAPHICS_GRID_SIZE as GRID_SIZE,
+  TURTLE_GRAPHICS_DEFAULT_COLOR as DEFAULT_COLOR,
 } from '../../constants';
 import { Box, Grid, GridItem, Image } from '../../infrastructures/useClient/chakra';
 import type { Problem } from '../../problems/generateProblem';
@@ -188,13 +189,13 @@ export const TurtleGraphics = forwardRef<TurtleGraphicsHandle, TurtleGraphicsPro
       updateCharacters(selectedCharacter);
     };
 
-    const handleAddCharacterButton = (color: ColorChar): void => {
-      if (!selectedCell || !color) return;
+    const handleAddCharacterButton = (): void => {
+      if (!selectedCell) return;
 
       const newTurtle = {
         x: selectedCell.x,
         y: selectedCell.y,
-        color,
+        color: DEFAULT_COLOR,
         dir: 'N',
         pen: true,
       };
@@ -215,12 +216,6 @@ export const TurtleGraphics = forwardRef<TurtleGraphicsHandle, TurtleGraphicsPro
 
       setSelectedCharacter(undefined);
       setSelectedCell({ x, y });
-    };
-
-    const handleChangeCellColorButton = (color: ColorChar): void => {
-      if (!selectedCell) return;
-
-      updateCellColor(color, selectedCell.x, selectedCell.y);
     };
 
     const handleContextMenu = (
@@ -279,9 +274,7 @@ export const TurtleGraphics = forwardRef<TurtleGraphicsHandle, TurtleGraphicsPro
         </Grid>
         {isEnableOperation && (
           <TurtleGraphicsController
-            board={board}
             handleAddCharacterButton={handleAddCharacterButton}
-            handleChangeCellColorButton={handleChangeCellColorButton}
             handleClickCharacterMoveForwardButton={handleClickCharacterMoveForwardButton}
             handleClickCharacterPenDownButton={handleClickCharacterPenDownButton}
             handleClickCharacterPenUpButton={handleClickCharacterPenUpButton}
