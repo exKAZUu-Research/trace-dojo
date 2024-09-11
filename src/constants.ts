e4c15c6a3b82a20a83ad4716c1329341606c978e
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-09-11 18:42:41 +0900

    change board size

--- src/constants.ts
+++ src/constants.ts
@@ -3,8 +3,8 @@ export const APP_DESCRIPTION = 'An educational web app for training trace skills
 export const APP_NAME = 'トレース道場';
 export const INTERVAL_MS_OF_IDLE_TIMER = 1000 * 60;
 
-export const TURTLE_GRAPHICS_GRID_COLUMNS = 13;
-export const TURTLE_GRAPHICS_GRID_ROWS = 9;
+export const TURTLE_GRAPHICS_GRID_COLUMNS = 7;
+export const TURTLE_GRAPHICS_GRID_ROWS = 7;
 export const TURTLE_GRAPHICS_GRID_SIZE = 40;
 export const TURTLE_GRAPHICS_EMPTY_COLOR = '.';
 export const TURTLE_GRAPHICS_DEFAULT_COLOR = '#';
