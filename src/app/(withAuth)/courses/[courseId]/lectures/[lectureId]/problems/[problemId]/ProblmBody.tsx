5b0e10ee4f9244588f676ff3aa88669c95c53563
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-09-26 11:25:28 +0900

    add stepMode label

--- src/app/(withAuth)/courses/[courseId]/lectures/[lectureId]/problems/[problemId]/ProblmBody.tsx
+++ src/app/(withAuth)/courses/[courseId]/lectures/[lectureId]/problems/[problemId]/ProblmBody.tsx
@@ -17,6 +17,7 @@ import {
   Heading,
   HStack,
   Icon,
+  Tag,
   Tooltip,
   useDisclosure,
   VStack,
@@ -119,7 +120,14 @@ export const ProblemBody: React.FC<Props> = (props) => {
       <VStack align="stretch" flexBasis={0} flexGrow={1} minW={0} spacing={4}>
         <VStack align="stretch" as={Card} overflow="hidden" spacing={0}>
           <VStack align="stretch" borderBottomWidth="1px" p={5}>
-            <Heading size="md">問題</Heading>
+            <HStack>
+              <Heading size="md">問題</Heading>
+              {problemType === 'step' && (
+                <Tag colorScheme="brand" fontWeight="bold" size="sm" variant="solid">
+                  ステップ実行モード
+                </Tag>
+              )}
+            </HStack>
 
             <div>
               <Box as="span" fontWeight="bold">
