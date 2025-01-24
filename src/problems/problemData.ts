f15c1d451919425801194e37299063dc3b3cd5f9
Sakamoto, Kazunori <exkazuu@gmail.com>
2025-01-24 09:32:07 +0900

    .

--- src/problems/problemData.ts
+++ src/problems/problemData.ts
@@ -7441,6 +7441,183 @@ public class Main {
 }
   `,
   },
+  twoDimensionalArray3: {
+    instrumented: `
+const FORWARD = 0;
+const TURN_RIGHT = 1;
+const TURN_LEFT = 2;
+
+const arr = [
+  [FORWARD, 3],
+  [TURN_RIGHT],
+  [FORWARD, 2],
+  [TURN_LEFT],
+  [FORWARD, 1]
+];
+const t = new Turtle(); // step
+
+for (s.set('i', 0); s.get('i') < arr.length; s.set('i', s.get('i') + 1)) { // step
+  call(moveTurtle, 't', 'command')(t, arr[s.get('i')]);
+}
+
+function moveTurtle(t, command) {
+  switch (command[0]) {
+    case FORWARD:
+      s.set('steps', command[1]); // step
+      for (s.set('j', 0); s.get('j') < s.get('steps'); s.set('j', s.get('j') + 1)) { // step
+        t.前に進む(); // step
+      }
+      delete s.vars['j'];
+      break;
+    case TURN_RIGHT:
+      t.右を向く(); // step
+      break;
+    case TURN_LEFT:
+      t.左を向く(); // step
+      break;
+  }
+}
+`,
+    java: `
+public class Main {
+  private static final int FORWARD = 0;
+  private static final int TURN_RIGHT = 1;
+  private static final int TURN_LEFT = 2;
+
+  public static void main(String[] args) {
+    int[][] arr = {
+      { FORWARD, 3 },
+      { TURN_RIGHT },
+      { FORWARD, 2 },
+      { TURN_LEFT },
+      { FORWARD, 1 }
+    };
+
+    Turtle t = new Turtle(); // step
+    for (int i = 0; i < arr.length; i++) { // step
+      moveTurtle(t, arr[i]); // caller
+    }
+  }
+
+  private static void moveTurtle(Turtle t, int[] command) {
+    switch (command[0]) {
+      case FORWARD:
+        int steps = command[1]; // step
+        for (int j = 0; j < steps; j++) { // step
+          t.前に進む(); // step
+        }
+        break;
+      case TURN_RIGHT:
+        t.右を向く(); // step
+        break;
+      case TURN_LEFT:
+        t.左を向く(); // step
+        break;
+    }
+  }
+}
+  `,
+  },
+  twoDimensionalArray4: {
+    instrumented: `
+const FORWARD = 0;
+const TURN_RIGHT = 1;
+const TURN_LEFT = 2;
+const PUT_TURTLE = 3;
+
+const arr = [
+  [PUT_TURTLE, 1, 2],
+  [FORWARD, 3],
+  [TURN_RIGHT],
+  [FORWARD, 2],
+  [TURN_LEFT],
+  [FORWARD, 1]
+];
+
+let turtle;
+for (s.set('i', 0); s.get('i') < arr.length; s.set('i', s.get('i') + 1)) { // step
+  s.set('command', arr[s.get('i')][0]); // step
+  switch (s.get('command')) {
+    case PUT_TURTLE:
+      turtle = new Turtle(arr[s.get('i')][1], arr[s.get('i')][2]); // step
+      break;
+    case FORWARD:
+      s.set('steps', arr[s.get('i')][1]); // step
+      for (s.set('j', 0); s.get('j') < s.get('steps'); s.set('j', s.get('j') + 1)) { // step
+        turtle.前に進む(); // step
+      }
+      delete s.vars['j'];
+      delete s.vars['steps'];
+      break;
+    case TURN_RIGHT:
+      turtle.右を向く(); // step
+      break;
+    case TURN_LEFT:
+      turtle.左を向く(); // step
+      break;
+  }
+  delete s.vars['command'];
+}
+delete s.vars['i'];
+`,
+    java: `
+public class Main {
+  private static final int FORWARD = 0;
+  private static final int TURN_RIGHT = 1;
+  private static final int TURN_LEFT = 2;
+  private static final int PUT_TURTLE = 3;
+
+  public static void main(String[] args) {
+    int[][] arr = {
+      { PUT_TURTLE, 1, 2 },
+      { FORWARD, 3 },
+      { TURN_RIGHT },
+      { FORWARD, 2 },
+      { TURN_LEFT },
+      { FORWARD, 1 }
+    };
+
+    Turtle turtle = null;
+    for (int i = 0; i < arr.length; i++) { // step
+      int command = arr[i][0]; // step
+      switch (command) {
+        case PUT_TURTLE:
+          turtle = new Turtle(arr[i][1], arr[i][2]); // step
+          break;
+        case FORWARD:
+          int steps = arr[i][1]; // step
+          for (int j = 0; j < steps; j++) { // step
+            t.前に進む(); // step
+          }
+          break;
+        case TURN_RIGHT:
+          t.右を向く(); // step
+          break;
+        case TURN_LEFT:
+          t.左を向く(); // step
+          break;
+      }
+    }
+  }
+}
+  `,
+  },
+  twoDimensionalArray5: {
+    instrumented: ``,
+    java: ``,
+  },
+  twoDimensionalArray6: {
+    instrumented: ``,
+    java: ``,
+  },
+  twoDimensionalArray7: {
+    instrumented: ``,
+    java: ``,
+  },
+  twoDimensionalArray8: {
+    instrumented: ``,
+    java: ``,
+  },
   threeDimensionalArray1: {
     instrumented: `
 s.set('a', [ [ [0, 0], [1, 1], [0, 3] ], [ [5, 2], [2, 1], [0, 4] ] ]);
@@ -7505,102 +7682,6 @@ public class Main {
   }
 }
   `,
-  },
-  twoDimensionalArray3: {
-    instrumented: `
-const FORWARD = 0;
-const TURN_RIGHT = 1;
-const TURN_LEFT = 2;
-
-const arr = [
-  [FORWARD, 3],
-  [TURN_RIGHT],
-  [FORWARD, 2],
-  [TURN_LEFT],
-  [FORWARD, 1]
-];
-const t = new Turtle(); // step
-
-for (s.set('i', 0); s.get('i') < arr.length; s.set('i', s.get('i') + 1)) { // step
-  call(moveTurtle, 't', 'command')(t, arr[s.get('i')]);
-}
-
-function moveTurtle(t, command) {
-  switch (command[0]) {
-    case FORWARD:
-      s.set('steps', command[1]); // step
-      for (s.set('j', 0); s.get('j') < s.get('steps'); s.set('j', s.get('j') + 1)) { // step
-        t.前に進む(); // step
-      }
-      break;
-    case TURN_RIGHT:
-      t.右を向く(); // step
-      break;
-    case TURN_LEFT:
-      t.左を向く(); // step
-      break;
-  }
-}
-`,
-    java: `
-public class Main {
-  private static final int FORWARD = 0;
-  private static final int TURN_RIGHT = 1;
-  private static final int TURN_LEFT = 2;
-
-  public static void main(String[] args) {
-    int[][] arr = {
-      { FORWARD, 3 },
-      { TURN_RIGHT },
-      { FORWARD, 2 },
-      { TURN_LEFT },
-      { FORWARD, 1 }
-    };
-    Turtle t = new Turtle(); // step
-
-    for (int i = 0; i < arr.length; i++) { // step
-      moveTurtle(t, arr[i]); // caller
-    }
-  }
-
-  private static void moveTurtle(Turtle t, int[] command) {
-    switch (command[0]) {
-      case FORWARD:
-        int steps = command[1]; // step
-        for (int j = 0; j < steps; j++) { // step
-          t.前に進む(); // step
-        }
-        break;
-      case TURN_RIGHT:
-        t.右を向く(); // step
-        break;
-      case TURN_LEFT:
-        t.左を向く(); // step
-        break;
-    }
-  }
-}
-  `,
-  },
-  twoDimensionalArray4: {
-    instrumented: ``,
-    java: ``,
-  },
-  twoDimensionalArray5: {
-    instrumented: ``,
-    java: ``,
-  },
-  twoDimensionalArray6: {
-    instrumented: ``,
-    java: ``,
-  },
-  twoDimensionalArray7: {
-    instrumented: ``,
-    java: ``,
-  },
-  twoDimensionalArray8: {
-    instrumented: ``,
-    java: ``,
   },
   threeDimensionalArray2: {
     instrumented: ``,
