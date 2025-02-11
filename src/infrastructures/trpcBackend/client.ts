a78ba49e4e5185586fdd96a964bc682bf74b2934
Sakamoto, Kazunori <exkazuu@gmail.com>
2025-02-11 13:50:17 +0900

    .

--- src/infrastructures/trpcBackend/client.ts
+++ src/infrastructures/trpcBackend/client.ts
@@ -1,6 +1,7 @@
 'use client';
 
-import { createTRPCReact, httpBatchLink } from '@trpc/react-query';
+import { httpBatchLink } from '@trpc/client';
+import { createTRPCReact } from '@trpc/react-query';
 import superjson from 'superjson';
 
 import type { BackendRouter } from './routers';
